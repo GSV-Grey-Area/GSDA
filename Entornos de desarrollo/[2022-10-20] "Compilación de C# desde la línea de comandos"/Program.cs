7c83d7fd7fbe21c7f18c13af6cf15a275c6b40b5
GSV-Grey-Area <556e6963726561746f72@gmail.com>
2022-10-20 09:19:39 +0200

    Add files via upload

---
+++ Entornos de desarrollo/[2022-10-20] "Compilación de C# desde la línea de comandos"/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Vientoporlamanana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Buenos días, usuario. ¿Cómo te llamas?");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola, " + nombre + ".");
+        }
+    }
+}
