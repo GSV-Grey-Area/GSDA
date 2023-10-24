5b6a7f48410a3dd5f293b40636cd1b8f835ea5e4
GSV-Grey-Area <556e6963726561746f72@gmail.com>
2023-10-24 13:09:46 +0200

    Create recogedatos.php

---
+++ Desarrollo Web en entorno servidor/[2023-10-14] "Cuadernillo Rubio"/recogedatos.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head>
+		<?php
+			echo "<style>";
+				echo ".red";
+				echo "{";
+					echo "color: red;";
+				echo "}";
+				
+				echo ".green";
+				echo "{";
+					echo "color: green;";
+				echo "}";
+			echo "</style>";
+		?>
+	</head>
+	<body>
+		<?php
+			$num1 = $_POST["num1"];
+			$num2 = $_POST["num2"];
+			$num3 = $_POST["num3"];
+						
+			$num4 = $num1 + $num2;
+			
+			echo "</p>El resultado es ".$num4.". ";
+			if ($num4 != $num3)
+			{
+				echo "<span class='red'>Has calculado mal.</span></p>";
+				echo "<form action='inicio.php' method='POST'></form>"
+			}
+			else if ($num4 = $num3)
+			{
+				echo "<span class='green'>Has calculado bien.</span></p>";
+			}
+			else
+			{
+				echo "<p>Algo ha ido mal.</p>";
+			}
+		?>
+	</body>
+</html>
