5af66d46ffd25ceccfe3473dbaa576585b5549da
GSV-Grey-Area <556e6963726561746f72@gmail.com>
2024-01-16 12:38:43 +0100

    Create Program.cs

---
+++ Programación/[2022-10-25] "Gato"/Program.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace Gato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (1 == 1)
+            {
+                Console.WriteLine("Miau");
+                Console.WriteLine("              //");
+                Console.WriteLine("/\\___ /\\       //");
+                Console.WriteLine("| O w O | ----//");
+                Console.WriteLine("| -------|     |");
+                Console.WriteLine("|| -----||-----||");
+                Console.WriteLine("||     |||    ||");
+                Console.WriteLine("||     |||    ||");
+                Console.WriteLine();
+
+                int a = 0;
+                while (a < 9500000)
+                {
+                    a++;
+                    Console.Write("");
+                }
+
+               
+                Console.WriteLine("\t\tMiau");
+                Console.WriteLine(" \\\\");
+                Console.WriteLine("\\\\        /\\___/\\");
+                Console.WriteLine("  \\\\---- | O w O |");
+                Console.WriteLine("  |     |------- |");
+                Console.WriteLine(" ||-----||----- ||");
+                Console.WriteLine("  ||    |||     ||");
+                Console.WriteLine("  ||    |||     ||");
+                Console.WriteLine();
+
+                int i = 0;
+                while(i<9500000)
+                {
+                    i++;
+                    Console.Write("");
+                }
+            }
+        }
+    }
+}
